efd10800b94496133da40c4a65388e779423f570
Igor Chaika <kp580bm1@gmail.com>
2014-01-24 23:10:41 +0200

    Minor optimization.

--- life.cpp
+++ life.cpp
@@ -2,21 +2,6 @@
 #include "life.h"
 const int HASH_COUNT = 10;
 
-template<class Iterator>
-unsigned hash(Iterator start, Iterator stop)
-{
-	unsigned h = 0, g = 0;
-	for(Iterator current = start; current != stop; ++current) {
-		h = (h << 4) + *current;
-		if((g = (h & 0xf0000000))) {
-			h ^= g >> 23;
-		}
-		h &= ~g;
-	}
-	return h;
-}
-
-
 Life::Life(int field_width, int field_height)
 	: width(field_width), height(field_height),
 	map(width * height, 0), buffer(width * height, 0),
@@ -26,6 +11,7 @@ Life::Life(int field_width, int field_height)
 
 void Life::tick()
 {
+	unsigned h = 0, g = 0;
 	for(int x = 0; x < width; x++) {
 		for(int y = 0; y < height; y++) {
 			int index = x + y * width;
@@ -45,13 +31,20 @@ void Life::tick()
 				map[left  + width * y     ];
 
 			// B3/S23
-			if(neighbours < 2 || neighbours > 3) {
-				buffer[index] = 0; // Die.
-			} else if(neighbours == 3 && map[index] == 0) {
-				buffer[index] = 1; // Live.
-			} else {
+			if(neighbours == 3) {
+				buffer[index] = 1;
+			} else if(neighbours == 2) {
 				buffer[index] = map[index];
+			} else {
+				buffer[index] = 0;
 			}
+
+			// Hash.
+			h = (h << 4) + buffer[index];
+			if((g = (h & 0xf0000000))) {
+				h ^= g >> 23;
+			}
+			h &= ~g;
 		}
 	}
 
@@ -59,7 +52,7 @@ void Life::tick()
 	map.swap(buffer);
 
 	// Loop detection.
-	unsigned int currentHash = hash(map.begin(), map.end());
+	unsigned int currentHash = h;
 	for(int i = 0; i < HASH_COUNT; i++) {
 		bool loopIsDetected = hashes[i] == currentHash;
 		if(loopIsDetected) {
